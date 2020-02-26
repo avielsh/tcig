6782d2026420151be4c2b64a80e5db47f783b7c8
Aviel shitrit <avielsh@gmail.com>
2020-02-26 12:02:48 +0200

    fix typos, local variables

--- tcig.sh
+++ tcig.sh
@@ -2,14 +2,14 @@
 cigfile="$HOME/cigs"
 now=$(gdate +'%m-%d %H:%M')
 today=$(echo $now | awk '{print $1}')
-msg_partial="Smooked partial"
-msg_one="Smooked one"
+msg_partial="Smoked partial"
+msg_one="Smoked one"
 OLDIFS="$IFS"
 
 date_diff() {
+  local old new sec_old sec_new
   old=$1
   new=$2
-  OLDIFS="$IFS"
   IFS=:
   echo "$old" | read old_hour old_min
   echo "$new" | read hour min
@@ -21,10 +21,11 @@ date_diff() {
 }
 
 last_cig_difference() {
+  local last thiscig diff
   last=$(tail -1 $cigfile | awk '{print $2}')
   thiscig=$(echo $now | awk '{print $2}')
   diff=$(date_diff $last $thiscig)
-  echo Last ciggarette was $diff minutes ago
+  echo Last cigarette was $diff minutes ago
 }
 
 addcig() {
@@ -38,20 +39,22 @@ addpartialcig() {
 }
 
 getstats() {
- # rev $cigfile | uniq -c -f 2 | rev
- total=$(awk '{print $1}' $cigfile | uniq -c)
- partial=$(grep $msg_partial $cigfile | awk '{print $1}' | uniq -c)
- one=$(grep $msg_one $cigfile | awk '{print $1}' | uniq -c)
- echo Total 
- echo "$total"  
- echo Partial
- echo "$partial"
- echo One
- echo "$one"
+  local total partial one
+  # rev $cigfile | uniq -c -f 2 | rev
+  total=$(awk '{print $1}' $cigfile | uniq -c)
+  partial=$(grep $msg_partial $cigfile | awk '{print $1}' | uniq -c)
+  one=$(grep $msg_one $cigfile | awk '{print $1}' | uniq -c)
+  echo Total 
+  echo "$total"  
+  echo Partial
+  echo "$partial"
+  echo One
+  echo "$one"
 }
 
 gettodaystats() {
   local alldiffs=()
+  local partial one alltimes diff alldiffs prevtime maxdiff mindiff
   partial=$(grep $msg_partial $cigfile | grep $today | awk '{print $1}' | uniq -c | awk '{print $1}')
   one=$(grep $msg_one $cigfile | grep $today | awk '{print $1}' | uniq -c | awk '{print $1}')
   alltimes=$(cat $cigfile | grep $today | awk '{print $2}')
